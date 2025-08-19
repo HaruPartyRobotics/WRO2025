3c08503cd274a36da14c32fd5d63da56caeca762
Nawfeel Rahman <1minutewithtechnology@gmail.com>
2025-08-19 21:04:02 +0600

    Model Creating Code Update
    Model Creating Code Update

--- Project Codes/CV Codes/ModelCreatingCode(YOLO)/ModelCreatingCode(YOLO).py
+++ Project Codes/CV Codes/ModelCreatingCode(YOLO)/ModelCreatingCode(YOLO).py
@@ -2,12 +2,38 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8s.pt")
 model.tune(
-    data=r"C:\Users\LENOVO\OneDrive\Documents Of LEARN\GitHub\WRO2025\Vehicles Dataset\Data.yaml",
-    epochs=10,
+    data="data.yaml",
+    epochs=50,
+    iterations=15,
+    optimizer="AdamW",
+    batch=4,
+    imgsz=640,
+    augment=False,
+    device="cpu",
+    plots=False,
+    val=False,
+    verbose=False,
 )
 model.train(
     data=r"C:\Users\LENOVO\OneDrive\Documents Of LEARN\GitHub\WRO2025\Vehicles Dataset\Data.yaml",
-    epochs=100,
-    imgsz=1024,
-    patience=5,
+    **model.tune_results.best_params,
+    data="data.yaml",
+    epochs=150,
+    batch=8,
+    imgsz=640,
+    augment=True,
+    hsv_h=0.0,
+    hsv_s=0.12,
+    hsv_v=0.12,
+    translate=0.18,
+    scale=0.22,
+    fliplr=0.25,
+    dropout=0.12,
+    patience=35,
+    mosaic=0.0,
+    mixup=0.0,
+    erasing=0.0,
+    device="cpu",
+    workers=0
 )
+model.val(data="data.yaml", batch=16, conf=0.45, iou=0.50)
