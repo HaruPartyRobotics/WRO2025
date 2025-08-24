32840a07c743b4154bd42f5a7c77de6796761128
Nawfeel Rahman <1minutewithtechnology@gmail.com>
2025-08-24 16:15:07 +0600

    Ambulance Counting Upd
    Ambulance Counting Upd

--- Project Codes/TrafficLightCode/TrafficLightAndSpikesCode/TrafficLightAndSpikesCode.ino
+++ Project Codes/TrafficLightCode/TrafficLightAndSpikesCode/TrafficLightAndSpikesCode.ino
@@ -1,151 +1,140 @@
-#include <ESP32Servo.h>
-Servo Servo1, Servo2, Servo3, Servo4;
-uint32_t t1, t2 = 0;
-int time1 = 5000, time2 = 5000;
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Servo.h>
+
+typedef struct struct_message {
+  uint8_t id;
+  bool b;
+} struct_message;
+
 #define l 100
 #define Servo_Pin1 19
 #define Servo_Pin2 22
 #define Servo_Pin3 23
 #define Servo_Pin4 15
-#define LDR_Pin1 33
-#define LDR_Pin2 34
-#define LDR_Pin3 35
-#define LDR_Pin4 36
-#define Side_0_Green 13
-#define Side_0_Yellow 17
-#define Side_0_Red 4
-#define Side_1_Green 27
-#define Side_1_Yellow 16
-#define Side_1_Red 18
-#define Limit 0 //Real One Got By Analyzing Real Sensor Values
-int f;
-int interval1 = 5000;
-int interval2 = 5000;
-unsigned long mls;
-int Flag;
+struct_message myDatar;
+Servo Servo1, Servo2, Servo3, Servo4;
+uint32_t mls, updt, mls2;
+int time1 = 5000, time2 = 5000;
+int led[6] = { 13, 17, 4, 27, 16, 18 };
+int interval1 = time1;
+int interval2 = time2;
+int curr, f;
 bool side = 0;
 String Input = "";
+bool Found = 0;
+
+int ambulanceCount = 0;
+int ambulanceIDs[20] = {};
+unsigned long lastSeen[20] = {};
+unsigned long lastCleanup = 0;
 
 void setup() {
-  pinMode(Side_0_Green, OUTPUT);
-  pinMode(Side_0_Yellow, OUTPUT);
-  pinMode(Side_0_Red, OUTPUT);
-  pinMode(Side_1_Green, OUTPUT);
-  pinMode(Side_1_Yellow, OUTPUT);
-  pinMode(Side_1_Red, OUTPUT);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    ESP.restart();
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+  for (byte i = 0; i < 6; i++) pinMode(led[i], OUTPUT);
   Servo1.attach(Servo_Pin1);
   Servo2.attach(Servo_Pin2);
   Servo3.attach(Servo_Pin3);
   Servo4.attach(Servo_Pin4);
-  t1 = t2 = millis();
-  Serial.begin(115200);
+  Servo1.write(90);
+  Servo2.write(90);
+  Servo3.write(0);
+  Servo4.write(0);
+  mls = millis();
+  (!side) ? curr = interval1 + mls - millis() : curr = interval2 + mls - millis();
 }
 
 void loop() {
-  mls = millis();
   while (Serial.available()) {
-    char c = Serial.read();
-    if (c == '\n') {
-      f = Input.toInt();
-      Input = "";
-    } else if (c != '\r') {
-      Input += c;
-    }
+    Input = Serial.readStringUntil(10);
+    f = Input.toInt();
   }
+
+  (!side) ? curr = interval1 + mls - millis() : curr = interval2 + mls - millis();
+
+  if (curr <= 0) {
+    side = !side;
+    Servo1.write(90 * (side == 0));
+    Servo2.write(90 * (side == 0));
+    Servo3.write(90 * (side == 1));
+    Servo4.write(90 * (side == 1));
+    digitalWrite(led[0], (side == 0));
+    digitalWrite(led[5], (side == 0));
+    digitalWrite(led[2], (side == 1));
+    digitalWrite(led[3], (side == 1));
+    signal_update();
+    mls = millis();  //timer reset
+  }
+
+  else if (curr < 1000) {  //for yellow light activation
+    for (byte i = 0; i < 6; i++) digitalWrite(led[i], 0);
+    digitalWrite(led[1], 1);
+    digitalWrite(led[4], 1);
+  }
+
+  if (millis() - updt > 100) {
+    Serial.print("Interval 1: ");
+    Serial.print(interval1);
+    Serial.print("  Interval 2: ");
+    Serial.print(interval2);
+    Serial.print("  Side: ");
+    Serial.print(side);
+    Serial.print("  Current: ");
+    Serial.println(curr);
+    updt = millis();
+  }
+
+  if (myDatar.id > 90 && myDatar.id < 99) {
+    addAmbulance(myDatar.id);
+  }
+
+  if(millis() - lastCleanup >= 5000){
+    cleanupAmbulances();
+    Serial.print("AMBULANCE_COUNT:");
+    Serial.println(ambulanceCount);
+    lastCleanup = millis();
+  }
+}
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&myDatar, incomingData, sizeof(myDatar));
+}
+
+void signal_update() {
   interval1 = time1 - (l * f);
   interval2 = time2 + (l * f);
   interval1 = constrain(interval1, 3000, 15000);
   interval2 = constrain(interval2, 3000, 15000);
-  int LDR_Reading1 = analogRead(LDR_Pin1);
-  int LDR_Reading2 = analogRead(LDR_Pin2);
-  int LDR_Reading3 = analogRead(LDR_Pin3);
-  int LDR_Reading4 = analogRead(LDR_Pin4);
-  if (!side) {
-    if (mls - t1 > (interval1 - 1000)) {
-      if (mls - t1 > interval1) {
-        side = 1;
-        t2 = millis();
-        Flag = 1;
-        digitalWrite(Side_0_Green, 0);
-        digitalWrite(Side_0_Yellow, 0);
-        digitalWrite(Side_0_Red, 1);
-        digitalWrite(Side_1_Green, 1);
-        digitalWrite(Side_1_Yellow, 0);
-        digitalWrite(Side_1_Red, 0);
-      } else {
-        Flag = 2;
-        digitalWrite(Side_0_Green, 0);
-        digitalWrite(Side_0_Yellow, 1);
-        digitalWrite(Side_0_Red,0);
-        digitalWrite(Side_1_Green, 0);
-        digitalWrite(Side_1_Yellow, 1);
-        digitalWrite(Side_1_Red, 0);
-      }
-    } else {
-      Flag = 3;
-        digitalWrite(Side_0_Green, 1);
-        digitalWrite(Side_0_Yellow, 0);
-        digitalWrite(Side_0_Red, 0);
-        digitalWrite(Side_1_Green, 0);
-        digitalWrite(Side_1_Yellow, 0);
-        digitalWrite(Side_1_Red, 1);
-    }
-  } else {
-    if (mls - t2 > (interval2 - 1000)) {
-      if (mls - t2 > interval2) {
-        side = 0;
-        Flag = 3;
-        t1 = millis();
-        digitalWrite(Side_0_Green, 1);
-        digitalWrite(Side_0_Yellow, 0);
-        digitalWrite(Side_0_Red, 0);
-        digitalWrite(Side_1_Green, 0);
-        digitalWrite(Side_1_Yellow, 0);
-        digitalWrite(Side_1_Red, 1);
-      } else {
-        Flag = 2;
-        digitalWrite(Side_0_Green, 0);
-        digitalWrite(Side_0_Yellow, 1);
-        digitalWrite(Side_0_Red, 0);
-        digitalWrite(Side_1_Green, 0);
-        digitalWrite(Side_1_Yellow, 1);
-        digitalWrite(Side_1_Red, 0);
-      }
-    } else {
-        Flag = 1;
-        digitalWrite(Side_0_Green, 0);
-        digitalWrite(Side_0_Yellow, 0);
-        digitalWrite(Side_0_Red, 1);
-        digitalWrite(Side_1_Green, 1);
-        digitalWrite(Side_1_Yellow, 0);
-        digitalWrite(Side_1_Red, 0);
+}
+
+void addAmbulance(uint8_t id) {
+  for(int i = 0; i < ambulanceCount; i++){
+    if(id == ambulanceIDs[i]){
+      lastSeen[i] = millis();
+      return;
     }
   }
-  if(LDR_Reading1 < Limit && Flag == 1){
-    Servo1.write(0);
+  if(ambulanceCount < 20){
+    ambulanceIDs[ambulanceCount] = id;
+    lastSeen[ambulanceCount] = millis();
+    ambulanceCount++;
   }
-  else if(LDR_Reading2 < Limit && Flag == 3){
-    Servo2.write(0);
+}
+
+void cleanupAmbulances() {
+  for(int i = 0; i < ambulanceCount; i++){
+    if(millis() - lastSeen[i] > 20000){
+      for(int j = i; j < ambulanceCount - 1; j++){
+        ambulanceIDs[j] = ambulanceIDs[j+1];
+        lastSeen[j] = lastSeen[j+1];
+      }
+      ambulanceCount--;
+      i--;
+    }
   }
-  else if(LDR_Reading3 < Limit && Flag == 1){
-    Servo3.write(0);
-  }
-  else if(LDR_Reading4 < Limit && Flag == 3){
-    Servo4.write(0);
-  }
-  else{
-    Servo1.write(90);
-    Servo2.write(90);
-    Servo3.write(90);
-    Servo4.write(90);
-  }
-  Serial.print("Interval 1: ");
-  Serial.println(interval1);
-  Serial.print("Side 0: ");
-  Serial.println(mls - t1);
-  Serial.print("Interval 2: ");
-  Serial.println(interval2);
-  Serial.println("Side 1: ");
-  Serial.println(mls - t2);
-  delay(100);
 }
