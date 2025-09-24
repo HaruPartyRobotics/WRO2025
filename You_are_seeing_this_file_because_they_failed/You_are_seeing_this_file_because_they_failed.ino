081bf7fd8dc0b2d8520bdc6832f92647a8e4595c
Fazle Elahi Tonmoy <fazleyelehi@gmail.com>
2025-09-25 03:37:15 +0600

    Update You_are_seeing_this_file_because_they_failed.ino

--- You_are_seeing_this_file_because_they_failed/You_are_seeing_this_file_because_they_failed.ino
+++ You_are_seeing_this_file_because_they_failed/You_are_seeing_this_file_because_they_failed.ino
@@ -1,11 +1,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define buzzerPin 25
+#define vehicle_ID 4
+
 #define pbPin 0
+#define buzzerPin 27
 #define nlight 26
+#define amb_button 25
 #define BUZZER_CHANNEL 0
-unsigned long presstime, releasetime, newpress, buzzerdelay, oldmls, pressed_timer = 0;
+unsigned long presstime, releasetime, newpress, buzzerdelay, oldmls, pressed_timer = 0, amb_interval;
 bool laststate = 1;
 bool state, buzzerstate = 0;
 int duration = 0;
@@ -25,14 +28,14 @@ struct_message vehicle;
 uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 esp_now_peer_info_t peerInfo;
 
-bool lastButtonState = false, rFlag = 0, lastHornState = 0, button_pressed = 0, long_pressed = 0;
+bool lastButtonState = false, rFlag = 0, lastHornState = 0, button_pressed = 0, long_pressed = 0, ambulance = 0;
 
 
 void setup() {
-  myData.id = 6;
   Serial.begin(115200);
   pinMode(buzzerPin, OUTPUT);
   pinMode(pbPin, INPUT_PULLUP);
+  pinMode(amb_button, INPUT_PULLUP);
   pinMode(nlight, OUTPUT);
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
@@ -45,72 +48,86 @@ void setup() {
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     ESP.restart();
   }
-  ledcSetup(BUZZER_CHANNEL, 2000, 8);
+  ledcSetup(BUZZER_CHANNEL, 1000, 8);
   ledcAttachPin(buzzerPin, BUZZER_CHANNEL);
+  ledcWriteTone(BUZZER_CHANNEL, 0);
+  for (byte i = 0; i < 3; i++) {
+    digitalWrite(nlight, 1);
+    delay(100);
+    digitalWrite(nlight, 0);
+    delay(100);
+  }
+
   newpress = millis();
 }
 
 void loop() {
   if (millis() - newpress > 20) {
     state = !digitalRead(pbPin);
+    ambulance = !digitalRead(amb_button);
     newpress = millis();
   }
+
   if (state && !button_pressed) {
     button_pressed = 1;
+    myData.id = vehicle_ID;
+    myData.b = 1;
+    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    Serial.println("Sent Data 1");
     presstime = millis();
   }
 
-  if (state && !long_pressed && millis() - presstime > 1000){
-    ledcWriteTone(BUZZER_CHANNEL, 5000);
+  if (state && !long_pressed && millis() - presstime > 1000) {
+    ledcWriteTone(BUZZER_CHANNEL, 3000);
+    Serial.println("Long Pressed!");
     long_pressed = 1;
   }
-  else if (state) {
-    myData.b = 1;
-    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-    Serial.printf("Sent Data 1");
-  }
-  else if(!state && button_pressed){
+
+  else if (!state && button_pressed) {
     button_pressed = 0;
-    if(long_pressed){
+    if (long_pressed) {
       ledcWriteTone(BUZZER_CHANNEL, 0);
       long_pressed = 0;
     }
-    else{
-      myData.b = 0;
-      esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-      Serial.printf("Sent Data 0");
-    }
+    myData.id = vehicle_ID;
+    myData.b = 0;
+    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    Serial.println("Sent Data 0");
   }
+
   if (rFlag) {
     rFlag = 0;
     Serial.printf("Car ID: %d -- Horn State: %d\n", vehicle.id, vehicle.b);
+
     if (vehicle.id < 90) {
       indexing();
       for (byte i = 0; i < 10; i++) Serial.printf("%d-", vehicle_index[0][i]);
       Serial.println();
       for (byte i = 0; i < 10; i++) Serial.printf("%d-", vehicle_index[1][i]);
       Serial.println();
-      ledcWriteTone(BUZZER_CHANNEL, 1000 + 200 * vehicle_sum);
+      ledcWriteTone(BUZZER_CHANNEL, 2000 * (vehicle_sum > 0) + 200 * vehicle_sum);
     }
 
-    else {
-      if (vehicle.b == 1) {
-        digitalWrite(nlight, 1);
-        if (millis() - oldmls < 100) {
-          ledcWriteTone(BUZZER_CHANNEL, 3000);
-        } else if (millis() - oldmls < 200) {
-          ledcWriteTone(BUZZER_CHANNEL, 0);
-        } else if (millis() - oldmls < 250) {
-          ledcWriteTone(BUZZER_CHANNEL, 3000);
-        } else if (millis() - oldmls < 300) {
-          ledcWriteTone(BUZZER_CHANNEL, 0);
-        } else {
-          ledcWriteTone(BUZZER_CHANNEL, 0);
-          digitalWrite(nlight, 0);
-          vehicle.b = 0;  // sequence finished
-          oldmls = millis();
-        }
-      }
+    else if (vehicle.b == 1) {
+      ledcWriteTone(BUZZER_CHANNEL, 3000);
+      digitalWrite(nlight, 1);
+      delay(100);
+      ledcWriteTone(BUZZER_CHANNEL, 0);
+      digitalWrite(nlight, 0);
+      delay(100);
+      ledcWriteTone(BUZZER_CHANNEL, 3000);
+      digitalWrite(nlight, 1);
+      delay(100);
+      ledcWriteTone(BUZZER_CHANNEL, 0);
+      digitalWrite(nlight, 0);
+      delay(100);
     }
+  }
+
+  if (millis() - amb_interval > 1000 && ambulance) {
+    myData.id = 91;
+    myData.b = 1;
+    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    amb_interval = millis();
   }
 }
